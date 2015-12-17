1005aeb9a66e8443b6e700bdfcfe54b805491065
remy0309 <stereal.0309@gmail.com>
2015-12-17 13:40:37 +0000

    saiteisyutu

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,5 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-  validates :age , inclusion: { in: 1..150 } , presence: true
+  validates :age , numericality: { greater_than_or_equal_to: 0 , only_integer: true } , presence: true
 end
